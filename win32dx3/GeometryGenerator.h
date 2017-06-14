9037545bfc9ae57abc18c710c28480962c5b2f61
Mike OShea <michaelo@attinc.com>
2017-06-14 13:43:20 -0700

    removed unused defines

--- win32dx3/GeometryGenerator.h
+++ win32dx3/GeometryGenerator.h
@@ -16,7 +16,6 @@
 #define GEOMETRYGENERATOR_H
 #include <windows.h>
 #include <d3d11_1.h>
-#include <d3dcompiler.h>
 #include <directxmath.h>
 #include <vector>
 
--- win32dx3/MathHelper.h
+++ win32dx3/MathHelper.h
@@ -9,7 +9,6 @@
 
 #include <windows.h>
 #include <d3d11_1.h>
-#include <d3dcompiler.h>
 #include <directxmath.h>
 
 using namespace DirectX;
