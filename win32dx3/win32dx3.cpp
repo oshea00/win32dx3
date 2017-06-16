e546c77d3fcfe5c27cffdb923d4b78d58aa73c12
Mike OShea <michaelo@attinc.com>
2017-06-16 11:09:55 -0700

    shapes demo done

--- win32dx3/win32dx3.cpp
+++ win32dx3/win32dx3.cpp
@@ -1,7 +1,7 @@
 //***************************************************************************************
-// win32dx3.cpp - Example app ported from 3D Game Programming Hills demo
+// ShapesDemo.cpp by Frank Luna (C) 2011 All Rights Reserved.
 //
-// Demonstrates drawing hills using a grid and 2D function to set the height of each vertex.
+// Demonstrates drawing simple geometric primitives in wireframe mode.
 //
 // Controls:
 //		Hold the left mouse button down and move the mouse to rotate.
@@ -20,11 +20,11 @@ struct Vertex
 	XMFLOAT4 Color;
 };
 
-class HillsApp : public D3DApp
+class ShapesApp : public D3DApp
 {
 public:
-	HillsApp(HINSTANCE hInstance);
-	~HillsApp();
+	ShapesApp(HINSTANCE hInstance);
+	~ShapesApp();
 
 	bool Init();
 	void OnResize();
@@ -36,7 +36,6 @@ public:
 	void OnMouseMove(WPARAM btnState, int x, int y);
 
 private:
-	float GetHeight(float x, float z)const;
 	void BuildGeometryBuffers();
 	void BuildFX();
 	void BuildVertexLayout();
@@ -51,13 +50,32 @@ private:
 
 	ID3D11InputLayout* mInputLayout;
 
-	// Define transformations from local spaces to world space.
-	XMFLOAT4X4 mGridWorld;
+	ID3D11RasterizerState* mWireframeRS;
 
-	UINT mGridIndexCount;
+	// Define transformations from local spaces to world space.
+	XMFLOAT4X4 mSphereWorld[10];
+	XMFLOAT4X4 mCylWorld[10];
+	XMFLOAT4X4 mBoxWorld;
+	XMFLOAT4X4 mGridWorld;
+	XMFLOAT4X4 mCenterSphere;
 
 	XMFLOAT4X4 mView;
 	XMFLOAT4X4 mProj;
+
+	int mBoxVertexOffset;
+	int mGridVertexOffset;
+	int mSphereVertexOffset;
+	int mCylinderVertexOffset;
+
+	UINT mBoxIndexOffset;
+	UINT mGridIndexOffset;
+	UINT mSphereIndexOffset;
+	UINT mCylinderIndexOffset;
+
+	UINT mBoxIndexCount;
+	UINT mGridIndexCount;
+	UINT mSphereIndexCount;
+	UINT mCylinderIndexCount;
 
 	float mTheta;
 	float mPhi;
@@ -74,7 +92,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	HillsApp theApp(hInstance);
+	ShapesApp theApp(hInstance);
 
 	if (!theApp.Init())
 		return 0;
@@ -83,12 +101,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 }
 
 
-HillsApp::HillsApp(HINSTANCE hInstance)
+ShapesApp::ShapesApp(HINSTANCE hInstance)
 	: D3DApp(hInstance), mVB(0), mIB(0), mFX(0), mTech(0),
-	mfxWorldViewProj(0), mInputLayout(0), mGridIndexCount(0),
-	mTheta(1.5f*MathHelper::Pi), mPhi(0.1f*MathHelper::Pi), mRadius(200.0f)
+	mfxWorldViewProj(0), mInputLayout(0), mWireframeRS(0),
+	mTheta(1.5f*MathHelper::Pi), mPhi(0.1f*MathHelper::Pi), mRadius(15.0f)
 {
-	mMainWndCaption = L"Hills Demo";
+	mMainWndCaption = L"Shapes Demo";
 
 	mLastMousePos.x = 0;
 	mLastMousePos.y = 0;
@@ -97,17 +115,35 @@ HillsApp::HillsApp(HINSTANCE hInstance)
 	XMStoreFloat4x4(&mGridWorld, I);
 	XMStoreFloat4x4(&mView, I);
 	XMStoreFloat4x4(&mProj, I);
+
+	XMMATRIX boxScale = XMMatrixScaling(2.0f, 1.0f, 2.0f);
+	XMMATRIX boxOffset = XMMatrixTranslation(0.0f, 0.5f, 0.0f);
+	XMStoreFloat4x4(&mBoxWorld, XMMatrixMultiply(boxScale, boxOffset));
+
+	XMMATRIX centerSphereScale = XMMatrixScaling(2.0f, 2.0f, 2.0f);
+	XMMATRIX centerSphereOffset = XMMatrixTranslation(0.0f, 2.0f, 0.0f);
+	XMStoreFloat4x4(&mCenterSphere, XMMatrixMultiply(centerSphereScale, centerSphereOffset));
+
+	for (int i = 0; i < 5; ++i)
+	{
+		XMStoreFloat4x4(&mCylWorld[i * 2 + 0], XMMatrixTranslation(-5.0f, 1.5f, -10.0f + i*5.0f));
+		XMStoreFloat4x4(&mCylWorld[i * 2 + 1], XMMatrixTranslation(+5.0f, 1.5f, -10.0f + i*5.0f));
+
+		XMStoreFloat4x4(&mSphereWorld[i * 2 + 0], XMMatrixTranslation(-5.0f, 3.5f, -10.0f + i*5.0f));
+		XMStoreFloat4x4(&mSphereWorld[i * 2 + 1], XMMatrixTranslation(+5.0f, 3.5f, -10.0f + i*5.0f));
+	}
 }
 
-HillsApp::~HillsApp()
+ShapesApp::~ShapesApp()
 {
 	ReleaseCOM(mVB);
 	ReleaseCOM(mIB);
 	ReleaseCOM(mFX);
 	ReleaseCOM(mInputLayout);
+	ReleaseCOM(mWireframeRS);
 }
 
-bool HillsApp::Init()
+bool ShapesApp::Init()
 {
 	if (!D3DApp::Init())
 		return false;
@@ -116,10 +152,19 @@ bool HillsApp::Init()
 	BuildFX();
 	BuildVertexLayout();
 
+	D3D11_RASTERIZER_DESC wireframeDesc;
+	ZeroMemory(&wireframeDesc, sizeof(D3D11_RASTERIZER_DESC));
+	wireframeDesc.FillMode = D3D11_FILL_WIREFRAME;
+	wireframeDesc.CullMode = D3D11_CULL_BACK;
+	wireframeDesc.FrontCounterClockwise = false;
+	wireframeDesc.DepthClipEnable = true;
+
+	HR(md3dDevice->CreateRasterizerState(&wireframeDesc, &mWireframeRS));
+
 	return true;
 }
 
-void HillsApp::OnResize()
+void ShapesApp::OnResize()
 {
 	D3DApp::OnResize();
 
@@ -127,7 +172,7 @@ void HillsApp::OnResize()
 	XMStoreFloat4x4(&mProj, P);
 }
 
-void HillsApp::UpdateScene(float dt)
+void ShapesApp::UpdateScene(float dt)
 {
 	// Convert Spherical to Cartesian coordinates.
 	float x = mRadius*sinf(mPhi)*cosf(mTheta);
@@ -143,7 +188,7 @@ void HillsApp::UpdateScene(float dt)
 	XMStoreFloat4x4(&mView, V);
 }
 
-void HillsApp::DrawScene()
+void ShapesApp::DrawScene()
 {
 	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::LightSteelBlue));
 	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
@@ -151,6 +196,7 @@ void HillsApp::DrawScene()
 	md3dImmediateContext->IASetInputLayout(mInputLayout);
 	md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
+	md3dImmediateContext->RSSetState(mWireframeRS);
 
 	UINT stride = sizeof(Vertex);
 	UINT offset = 0;
@@ -161,23 +207,53 @@ void HillsApp::DrawScene()
 
 	XMMATRIX view = XMLoadFloat4x4(&mView);
 	XMMATRIX proj = XMLoadFloat4x4(&mProj);
-	XMMATRIX world = XMLoadFloat4x4(&mGridWorld);
-	XMMATRIX worldViewProj = world*view*proj;
+	XMMATRIX viewProj = view*proj;
 
 	D3DX11_TECHNIQUE_DESC techDesc;
 	mTech->GetDesc(&techDesc);
 	for (UINT p = 0; p < techDesc.Passes; ++p)
 	{
 		// Draw the grid.
-		mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&worldViewProj));
+		XMMATRIX world = XMLoadFloat4x4(&mGridWorld);
+		mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&(world*viewProj)));
 		mTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
-		md3dImmediateContext->DrawIndexed(mGridIndexCount, 0, 0);
+		md3dImmediateContext->DrawIndexed(mGridIndexCount, mGridIndexOffset, mGridVertexOffset);
+
+		// Draw the box.
+		world = XMLoadFloat4x4(&mBoxWorld);
+		mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&(world*viewProj)));
+		mTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
+		md3dImmediateContext->DrawIndexed(mBoxIndexCount, mBoxIndexOffset, mBoxVertexOffset);
+
+		// Draw center sphere.
+		world = XMLoadFloat4x4(&mCenterSphere);
+		mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&(world*viewProj)));
+		mTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
+		md3dImmediateContext->DrawIndexed(mSphereIndexCount, mSphereIndexOffset, mSphereVertexOffset);
+
+		// Draw the cylinders.
+		for (int i = 0; i < 10; ++i)
+		{
+			world = XMLoadFloat4x4(&mCylWorld[i]);
+			mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&(world*viewProj)));
+			mTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
+			md3dImmediateContext->DrawIndexed(mCylinderIndexCount, mCylinderIndexOffset, mCylinderVertexOffset);
+		}
+
+		// Draw the spheres.
+		for (int i = 0; i < 10; ++i)
+		{
+			world = XMLoadFloat4x4(&mSphereWorld[i]);
+			mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&(world*viewProj)));
+			mTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
+			md3dImmediateContext->DrawIndexed(mSphereIndexCount, mSphereIndexOffset, mSphereVertexOffset);
+		}
 	}
 
 	HR(mSwapChain->Present(0, 0));
 }
 
-void HillsApp::OnMouseDown(WPARAM btnState, int x, int y)
+void ShapesApp::OnMouseDown(WPARAM btnState, int x, int y)
 {
 	mLastMousePos.x = x;
 	mLastMousePos.y = y;
@@ -185,12 +261,12 @@ void HillsApp::OnMouseDown(WPARAM btnState, int x, int y)
 	SetCapture(mhMainWnd);
 }
 
-void HillsApp::OnMouseUp(WPARAM btnState, int x, int y)
+void ShapesApp::OnMouseUp(WPARAM btnState, int x, int y)
 {
 	ReleaseCapture();
 }
 
-void HillsApp::OnMouseMove(WPARAM btnState, int x, int y)
+void ShapesApp::OnMouseMove(WPARAM btnState, int x, int y)
 {
 	if ((btnState & MK_LBUTTON) != 0)
 	{
@@ -207,82 +283,102 @@ void HillsApp::OnMouseMove(WPARAM btnState, int x, int y)
 	}
 	else if ((btnState & MK_RBUTTON) != 0)
 	{
-		// Make each pixel correspond to 0.2 unit in the scene.
-		float dx = 0.2f*static_cast<float>(x - mLastMousePos.x);
-		float dy = 0.2f*static_cast<float>(y - mLastMousePos.y);
+		// Make each pixel correspond to 0.01 unit in the scene.
+		float dx = 0.01f*static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.01f*static_cast<float>(y - mLastMousePos.y);
 
 		// Update the camera radius based on input.
 		mRadius += dx - dy;
 
 		// Restrict the radius.
-		mRadius = MathHelper::Clamp(mRadius, 50.0f, 500.0f);
+		mRadius = MathHelper::Clamp(mRadius, 3.0f, 200.0f);
 	}
 
 	mLastMousePos.x = x;
 	mLastMousePos.y = y;
 }
 
-float HillsApp::GetHeight(float x, float z)const
+void ShapesApp::BuildGeometryBuffers()
 {
-	return 0.3f*(z*sinf(0.1f*x) + x*cosf(0.1f*z));
-}
-
-void HillsApp::BuildGeometryBuffers()
-{
+	GeometryGenerator::MeshData box;
 	GeometryGenerator::MeshData grid;
+	GeometryGenerator::MeshData sphere;
+	GeometryGenerator::MeshData cylinder;
 
 	GeometryGenerator geoGen;
+	geoGen.CreateBox(1.0f, 1.0f, 1.0f, box);
+	geoGen.CreateGrid(20.0f, 30.0f, 60, 40, grid);
+	geoGen.CreateSphere(0.5f, 20, 20, sphere);
+	//geoGen.CreateGeosphere(0.5f, 2, sphere);
+	geoGen.CreateCylinder(0.5f, 0.3f, 3.0f, 20, 20, cylinder);
 
-	geoGen.CreateGrid(160.0f, 160.0f, 50, 50, grid);
+	// Cache the vertex offsets to each object in the concatenated vertex buffer.
+	mBoxVertexOffset = 0;
+	mGridVertexOffset = box.Vertices.size();
+	mSphereVertexOffset = mGridVertexOffset + grid.Vertices.size();
+	mCylinderVertexOffset = mSphereVertexOffset + sphere.Vertices.size();
 
+	// Cache the index count of each object.
+	mBoxIndexCount = box.Indices.size();
 	mGridIndexCount = grid.Indices.size();
+	mSphereIndexCount = sphere.Indices.size();
+	mCylinderIndexCount = cylinder.Indices.size();
+
+	// Cache the starting index for each object in the concatenated index buffer.
+	mBoxIndexOffset = 0;
+	mGridIndexOffset = mBoxIndexCount;
+	mSphereIndexOffset = mGridIndexOffset + mGridIndexCount;
+	mCylinderIndexOffset = mSphereIndexOffset + mSphereIndexCount;
+
+	UINT totalVertexCount =
+		box.Vertices.size() +
+		grid.Vertices.size() +
+		sphere.Vertices.size() +
+		cylinder.Vertices.size();
+
+	UINT totalIndexCount =
+		mBoxIndexCount +
+		mGridIndexCount +
+		mSphereIndexCount +
+		mCylinderIndexCount;
 
 	//
-	// Extract the vertex elements we are interested and apply the height function to
-	// each vertex.  In addition, color the vertices based on their height so we have
-	// sandy looking beaches, grassy low hills, and snow mountain peaks.
+	// Extract the vertex elements we are interested in and pack the
+	// vertices of all the meshes into one vertex buffer.
 	//
 
-	std::vector<Vertex> vertices(grid.Vertices.size());
-	for (size_t i = 0; i < grid.Vertices.size(); ++i)
+	std::vector<Vertex> vertices(totalVertexCount);
+
+	XMFLOAT4 black(0.0f, 0.0f, 0.0f, 1.0f);
+
+	UINT k = 0;
+	for (size_t i = 0; i < box.Vertices.size(); ++i, ++k)
 	{
-		XMFLOAT3 p = grid.Vertices[i].Position;
+		vertices[k].Pos = box.Vertices[i].Position;
+		vertices[k].Color = black;
+	}
 
-		p.y = GetHeight(p.x, p.z);
+	for (size_t i = 0; i < grid.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = grid.Vertices[i].Position;
+		vertices[k].Color = black;
+	}
 
-		vertices[i].Pos = p;
+	for (size_t i = 0; i < sphere.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = sphere.Vertices[i].Position;
+		vertices[k].Color = black;
+	}
 
-		// Color the vertex based on its height.
-		if (p.y < -10.0f)
-		{
-			// Sandy beach color.
-			vertices[i].Color = XMFLOAT4(1.0f, 0.96f, 0.62f, 1.0f);
-		}
-		else if (p.y < 5.0f)
-		{
-			// Light yellow-green.
-			vertices[i].Color = XMFLOAT4(0.48f, 0.77f, 0.46f, 1.0f);
-		}
-		else if (p.y < 12.0f)
-		{
-			// Dark yellow-green.
-			vertices[i].Color = XMFLOAT4(0.1f, 0.48f, 0.19f, 1.0f);
-		}
-		else if (p.y < 20.0f)
-		{
-			// Dark brown.
-			vertices[i].Color = XMFLOAT4(0.45f, 0.39f, 0.34f, 1.0f);
-		}
-		else
-		{
-			// White snow.
-			vertices[i].Color = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-		}
+	for (size_t i = 0; i < cylinder.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = cylinder.Vertices[i].Position;
+		vertices[k].Color = black;
 	}
 
 	D3D11_BUFFER_DESC vbd;
 	vbd.Usage = D3D11_USAGE_IMMUTABLE;
-	vbd.ByteWidth = sizeof(Vertex) * grid.Vertices.size();
+	vbd.ByteWidth = sizeof(Vertex) * totalVertexCount;
 	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	vbd.CPUAccessFlags = 0;
 	vbd.MiscFlags = 0;
@@ -294,18 +390,24 @@ void HillsApp::BuildGeometryBuffers()
 	// Pack the indices of all the meshes into one index buffer.
 	//
 
+	std::vector<UINT> indices;
+	indices.insert(indices.end(), box.Indices.begin(), box.Indices.end());
+	indices.insert(indices.end(), grid.Indices.begin(), grid.Indices.end());
+	indices.insert(indices.end(), sphere.Indices.begin(), sphere.Indices.end());
+	indices.insert(indices.end(), cylinder.Indices.begin(), cylinder.Indices.end());
+
 	D3D11_BUFFER_DESC ibd;
 	ibd.Usage = D3D11_USAGE_IMMUTABLE;
-	ibd.ByteWidth = sizeof(UINT) * mGridIndexCount;
+	ibd.ByteWidth = sizeof(UINT) * totalIndexCount;
 	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
 	ibd.CPUAccessFlags = 0;
 	ibd.MiscFlags = 0;
 	D3D11_SUBRESOURCE_DATA iinitData;
-	iinitData.pSysMem = &grid.Indices[0];
+	iinitData.pSysMem = &indices[0];
 	HR(md3dDevice->CreateBuffer(&ibd, &iinitData, &mIB));
 }
 
-void HillsApp::BuildFX()
+void ShapesApp::BuildFX()
 {
 	std::ifstream fin("color.fxo", std::ios::binary);
 
@@ -324,7 +426,7 @@ void HillsApp::BuildFX()
 	mfxWorldViewProj = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
 }
 
-void HillsApp::BuildVertexLayout()
+void ShapesApp::BuildVertexLayout()
 {
 	// Create the vertex input layout.
 	D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
